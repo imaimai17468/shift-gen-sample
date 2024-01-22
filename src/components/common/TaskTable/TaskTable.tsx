6250acc1196239b200b0b05a97fc9f8a97a1f52c
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-22 18:14:34 +0900

    feat: Add TaskTable component with dynamic table generation

--- src/components/common/TaskTable/TaskTable.tsx
+++ src/components/common/TaskTable/TaskTable.tsx
@@ -1,10 +1,43 @@
 import { TimeTable } from "../TimeTable";
+import { useState, useMemo } from "react";
+import { genDefaultTaskTimelines } from "../../../utils/genDefaultTaskTimelines";
+import { TaskTimelines } from "@/types/TaskTimelines";
+import { SHIFT_TIMES } from "@/constants/ShiftTimes";
+import { Table } from "@mantine/core";
 
 export const TaskTable = () => {
+  const [taskTimelines, setTaskTimelines] = useState<TaskTimelines>(
+    genDefaultTaskTimelines()
+  );
+  const TaskNames = useMemo(() => {
+    return taskTimelines.map((timeline) => timeline.task);
+  }, [taskTimelines]);
+
   return (
     <div className="flex gap-2">
       <TimeTable />
-      <p>content</p>
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            {TaskNames.map((TaskName) => (
+              <Table.Th key={TaskName} className="whitespace-nowrap">
+                {TaskName}
+              </Table.Th>
+            ))}
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {SHIFT_TIMES.map((time, index) => (
+            <Table.Tr key={time}>
+              {taskTimelines.map((timeline) => (
+                <Table.Td key={timeline.task} className="whitespace-nowrap text-center">
+                  {timeline.timeline[index].required_personnel}
+                </Table.Td>
+              ))}
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
     </div>
   );
 };
