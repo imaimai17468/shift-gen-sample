467aea6fc9cabde211cf32ff0216d69c0389e869
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-22 16:37:51 +0900

    feat: Add shift and task tables to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <h1>Shift-Gen-Sample</h1>
+      <div className="flex gap-4">
+        <div>
+          <h2>Shift Table</h2>
+        </div>
+        <div>
+          <h2>Task Table</h2>
+        </div>
+      </div>
     </div>
   );
 }
