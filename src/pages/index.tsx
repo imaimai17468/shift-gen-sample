b445ca750ee74f520d72bc90ff8468a2e264c19e
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-22 16:47:54 +0900

    feat: Update index page with ShiftTable and TaskTable components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
+import { ShiftTable } from "@/components/common/ShiftTable";
+import { TaskTable } from "@/components/common/TaskTable";
+
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4">
-      <h1>Shift-Gen-Sample</h1>
+    <div className="flex flex-col gap-4 p-4">
+      <h1 className="text-2xl font-bold">Shift-Gen-Sample</h1>
       <div className="flex gap-4">
         <div>
-          <h2>Shift Table</h2>
+          <h2 className="text-xl">Shift Table</h2>
+          <ShiftTable />
         </div>
         <div>
-          <h2>Task Table</h2>
+          <h2 className="text-xl">Task Table</h2>
+          <TaskTable />
         </div>
       </div>
     </div>
