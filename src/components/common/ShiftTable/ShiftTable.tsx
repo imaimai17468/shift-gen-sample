563359e1ed80c607d6d3c59d1fb07334d246ea1f
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-22 18:07:27 +0900

    feat: Add user timelines to ShiftTable component

--- src/components/common/ShiftTable/ShiftTable.tsx
+++ src/components/common/ShiftTable/ShiftTable.tsx
@@ -1,10 +1,42 @@
 import { TimeTable } from "../TimeTable";
+import { UserTimelines } from "@/types/UserTimelines";
+import { genDefaultUserTimelines } from "@/utils/genDefaultUserTimelines";
+import { useState, useMemo } from "react";
+import { Table } from "@mantine/core";
+import { SHIFT_TIMES } from "@/constants/ShiftTimes";
 
 export const ShiftTable = () => {
+  const [userTimelines, setUserTimelines] = useState<UserTimelines>(
+    genDefaultUserTimelines()
+  );
+
+  const userNames = useMemo(() => {
+    return userTimelines.map((timeline) => timeline.user);
+  }, [userTimelines]);
+
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 items-start">
       <TimeTable />
-      <p>content</p>
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            {userNames.map((userName) => (
+              <Table.Th key={userName}>{userName}</Table.Th>
+            ))}
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {SHIFT_TIMES.map((time, index) => (
+            <Table.Tr key={time}>
+              {userTimelines.map((timeline) => (
+                <Table.Td key={timeline.user} className="whitespace-nowrap">
+                  {timeline.timeline[index].task || "なし"}
+                </Table.Td>
+              ))}
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
     </div>
   );
 };
