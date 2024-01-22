22b27c52a6cee380ae683bfce5579ed1ca595a0a
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-22 22:19:17 +0900

    feat: Refactor component imports and add shift generation feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,74 @@
 import { ShiftTable } from "@/components/common/ShiftTable";
 import { TaskTable } from "@/components/common/TaskTable";
-import { TaskTimelines } from "@/types/TaskTimelines";
-import { UserTimelines } from "@/types/UserTimelines";
-import { useState } from "react";
+import { TaskTimeline } from "@/types/TaskTimeline";
+import { UserTimeline } from "@/types/UserTimeline";
+import { useState, useMemo } from "react";
 import { genDefaultTaskTimelines } from "@/utils/genDefaultTaskTimelines";
 import { genDefaultUserTimelines } from "@/utils/genDefaultUserTimelines";
+import { Button } from "@mantine/core";
+import { combination } from "@/utils/combination";
+import { SHIFT_TIMES } from "@/constants/ShiftTimes";
 
 export default function Home() {
-  const [userTimelines, setUserTimelines] = useState<UserTimelines>(
+  const [userTimelines, setUserTimelines] = useState<UserTimeline[]>(
     genDefaultUserTimelines()
   );
-  const [taskTimelines, setTaskTimelines] = useState<TaskTimelines>(
+  const [taskTimelines, setTaskTimelines] = useState<TaskTimeline[]>(
     genDefaultTaskTimelines()
   );
 
+  const handleGenerateShift = () => {
+    const userNames = userTimelines.map((userTimeline) => userTimeline.user);
+
+    let newUserTimelines: UserTimeline[] = userTimelines.map((userTimeline) => {
+      return {
+        ...userTimeline,
+        timeline: userTimeline.timeline.map((timeline) => {
+          return {
+            ...timeline,
+            task: null,
+          };
+        }),
+      };
+    });
+
+    SHIFT_TIMES.forEach((_, index) => {
+      let noTaskUsers = [...userNames];
+      taskTimelines.forEach((taskTimeline) => {
+        const taskName = taskTimeline.task;
+        const requiredPersonnel =
+          taskTimeline.timeline[index].required_personnel;
+
+        if (requiredPersonnel === 0 || noTaskUsers.length === 0) {
+          return;
+        }
+
+        const combinationUsers = combination(noTaskUsers, requiredPersonnel)[0];
+        combinationUsers.forEach((user) => {
+          console.log(user);
+          const userTimelineIndex = newUserTimelines.findIndex(
+            (userTimeline) => userTimeline.user === user
+          );
+          newUserTimelines[userTimelineIndex].timeline[index].task = taskName;
+        });
+        combinationUsers.forEach((user) => {
+          const userIndex = noTaskUsers.findIndex((noTaskUser) => {
+            return noTaskUser === user;
+          });
+          noTaskUsers.splice(userIndex, 1);
+        });
+      });
+    });
+
+    setUserTimelines(newUserTimelines);
+  };
+
   return (
     <div className="flex flex-col gap-8 p-4">
-      <h1 className="text-2xl font-bold">Shift-Gen-Sample</h1>
+      <div className="flex gap-4">
+        <h1 className="text-2xl font-bold">Shift-Gen-Sample</h1>
+        <Button onClick={handleGenerateShift}>Generate Shift</Button>
+      </div>
       <div className="flex gap-8">
         <ShiftTable
           userTimelines={userTimelines}
